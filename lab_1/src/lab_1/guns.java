ec80398a5da8b6bc8ea1ee66f16c0adff672c580
nickiteks <splint38@gmail.com>
2019-12-19 21:44:06 +0300

    finish

---
+++ lab_1/src/lab_1/guns.java
@@ -0,0 +1,7 @@
+package lab_1;
+
+public enum  guns {
+	powerGun,
+	superGun,
+	ultra_superGun,
+}
---
+++ lab_1/src/lab_1/gunsDraw.java
@@ -0,0 +1,32 @@
+package lab_1;
+
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class gunsDraw {
+	guns guns;
+	Color colorGun;
+	public gunsDraw(guns guns, Color _colorGun) {
+		this.colorGun=_colorGun;
+		this.guns=guns;
+	}	
+	public void draw(Graphics g, int startX, int startY) {
+		int size=5;
+		g.setColor(colorGun);
+		switch (guns)
+		{
+		case ultra_superGun:			
+			g.drawRect( startX + 5, startY + size, 85, 5);		
+			g.fillRect( startX + 5, startY + size, 85, 5);			
+			size+=9;		
+		case superGun:		
+			g.drawRect( startX + 5, startY + size, 85, 5);		
+			g.fillRect( startX + 5, startY + size, 85, 5);			
+			size+=9;		
+		case powerGun:			
+			g.drawRect( startX + 5, startY + size, 85, 5);			
+			g.fillRect( startX + 5, startY + size, 85, 5);				
+			break;						
+		}			
+	}
+}
