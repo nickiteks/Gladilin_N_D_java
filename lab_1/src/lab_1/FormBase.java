26fbe3752fbd85b3962fe59fa3204dcdca6422ff
nickiteks <splint38@gmail.com>
2020-01-16 00:13:50 +0300

    final

---
+++ lab_1/src/lab_1/FormBase.java
@@ -0,0 +1,145 @@
+package lab_1;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
+
+import java.awt.event.ActionListener;
+import java.util.Random;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextField;
+
+public class FormBase {
+	private JFrame frame;
+	private WarBasePanel pictureBoxBig;
+	Random rnd = new Random();
+	private JTextField textField;
+	ITransport transport=null;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FormBase window = new FormBase();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public FormBase() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 988, 776);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		WarBase<ITransport, IGuns> base= new WarBase<ITransport ,IGuns>(20, 663, 440);	
+		pictureBoxBig = new WarBasePanel(base);
+		pictureBoxBig.setBounds(22, 21, 719, 705);
+		frame.getContentPane().add(pictureBoxBig);
+		JButton button_Car = new JButton("\u043C\u0430\u0448\u0438\u043D\u0430");
+		button_Car.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			   Color mainColor = JColorChooser.showDialog(pictureBoxBig, "chose", Color.BLACK);
+				transport = new WarCar(rnd.nextInt(20)+100, rnd.nextInt(1000)+1000, mainColor);		
+				 base.clone(mainColor, null, 0);
+				base.add(transport);							
+			}
+		});
+		button_Car.setBounds(829, 55, 126, 40);
+		frame.getContentPane().add(button_Car);
+		
+		JButton button_Tank = new JButton("\u0442\u0430\u043D\u043A");
+		button_Tank.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {			
+				Color mainColor = JColorChooser.showDialog(pictureBoxBig, "chose", Color.BLACK);
+				Color DopColor = JColorChooser.showDialog(pictureBoxBig, "chose", Color.BLACK);
+				 base.clone(mainColor, DopColor, 1);			 
+				transport = new tank(rnd.nextInt(20)+100,
+						guns.superGun, 
+						rnd.nextInt(1000)+1000, 
+						mainColor,
+						DopColor,
+				        false, 
+				        false, 
+				        false);				
+	guns _guns = guns.superGun;
+								
+	IGuns gunsForm = new gunsDraw(_guns, DopColor); ;				
+				switch (rnd.nextInt() % 3)
+				{
+				case 1:
+				gunsForm = new gunsDraw(_guns, DopColor);
+				break;
+				case 2 : 
+				gunsForm = new OvalGuns(_guns, DopColor);
+					break;
+				case 3:
+				gunsForm = new LargeGuns(_guns, DopColor);
+					break;					
+				}							
+				base.add(transport,gunsForm);           				
+			}
+		});
+		button_Tank.setBounds(829, 106, 126, 40);
+		frame.getContentPane().add(button_Tank);
+		
+		JPanel panel_Exit = new ExitPanel();
+		panel_Exit.setBounds(751, 367, 205, 158);
+		frame.getContentPane().add(panel_Exit);
+		
+		JButton button_TakeCar = new JButton("\u0437\u0430\u0431\u0440\u0430\u0442\u044C");
+		button_TakeCar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {								
+				transport = base.sub(Integer.valueOf(textField.getText()));				
+				transport.SetPosition(40, 10, ExitPanel.WIDTH, ExitPanel.HEIGHT);
+				 ((ExitPanel) panel_Exit).addTransport(transport);				
+			}
+		});
+		button_TakeCar.setBounds(847, 327, 89, 23);
+		frame.getContentPane().add(button_TakeCar);
+		
+		textField = new JTextField();
+		textField.setBounds(847, 296, 96, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JButton Button_multy = new JButton("\u0443\u043C\u043D\u043E\u0436\u0435\u043D\u0438\u0435");
+		Button_multy.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+		base.mylty(2);
+		pictureBoxBig.repaint();				
+			}
+		});
+		Button_multy.setBounds(829, 157, 126, 40);
+		frame.getContentPane().add(Button_multy);
+		
+		JButton Button_Del = new JButton("\u0434\u0435\u043B\u0435\u043D\u0438\u0435");
+		Button_Del.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {				
+				base.del(2);
+				pictureBoxBig.repaint();
+			}
+		});
+		Button_Del.setBounds(829, 205, 126, 40);
+		frame.getContentPane().add(Button_Del);				
+	}
+}
---
+++ lab_1/src/lab_1/WarBase.java
@@ -0,0 +1,177 @@
+package lab_1;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
+public class WarBase <T extends Object&ITransport , W extends Object&IGuns> {
+	
+		private T[] _places;
+		
+		private W[] gunsForm;
+	
+	    private int PictureWidth;
+
+	    private int PictureHeight;
+	   
+	    private final int _placeSizeWidth = 270;
+
+	    private final int _placeSizeHeight = 135;
+	    
+	    int type;
+	    
+	    private  Color mainColor;
+	    
+	    private  Color dopColor;
+	    	
+	    public WarBase(int sizes, int pictureWidth, int pictureHeight)
+	    {    	
+	    	_places = (T[]) new Object[sizes];
+	    	gunsForm = (W[]) new Object[sizes];
+	    	PictureWidth = pictureWidth;	    	
+	    	 PictureHeight = pictureHeight;    		    
+	    }
+	    
+	    public void clone(Color mainColor ,Color dopColor,int type)
+	    {    	
+	    	 this.mainColor =mainColor;
+	    	 this.dopColor= dopColor;
+	    	 this.type=type;	    		    
+	    }
+	    
+	    public int add (ITransport transport)
+	    {
+	        for (int i = 0; i < this._places.length; i++)
+	        {
+	            if (this.CheckFreePlace(i))
+	            {
+	            	this._places[i] = (T) transport;
+	                this._places[i].SetPosition(30 + i / 5 * _placeSizeWidth + 5,
+	                                             i % 5 * _placeSizeHeight + 15,
+	                                             this.PictureWidth,
+	                                             this.PictureHeight);
+	                return i;
+	            }
+	        }
+	        return -1;
+	    }
+	        
+	    public int add(T transport, W gunsForm)
+	    {
+	        for (int i = 0; i < this._places.length; i++)
+	        {
+	            if (this.CheckFreePlace(i))
+	            {
+	            	this._places[i] =  transport;
+	            	this._places[i].SetPosition(30 + i / 5 * _placeSizeWidth + 5,
+	                                             i % 5 * _placeSizeHeight + 15,
+	                                             this.PictureWidth,
+	                                             this.PictureHeight);
+	            	this.gunsForm[i] = gunsForm;
+	                return i;
+	            }
+	        }
+	        return -1;
+	    }
+	   	    
+	    public T sub (int index)
+	    {
+	        if (index < 0 || index > this._places.length)
+	        {
+	            return null;
+	        }
+	        if (!this.CheckFreePlace(index))
+	        {
+	            T machine = this._places[index];
+	            this._places[index] = null;
+	            return machine;
+	        }     
+	        return null;
+	    }
+	       
+	    public  int   mylty(int num) 
+	    {
+	    	Random rnd = new Random(); 	
+	    	 int score=0;  
+	    	for (int i = 0; i < this._places.length; i++)
+	        {
+	            if (!this.CheckFreePlace(i))
+	            {
+	            	score++;
+	            }
+	        }    	
+	    	num = (num*score)-score;   	
+	    	for (int j = 0; j < num; j++) 
+	    	{			    	
+		    	ITransport _transport =null;		    	
+		    	if(type==0) 
+		    	{
+		    		_transport = new WarCar(rnd.nextInt(20)+100, rnd.nextInt(1000)+1000, mainColor); 			    		
+		    	}		    	
+		    	if(type==1) 
+		    	{		    		
+		    		_transport = new tank(rnd.nextInt(20)+100,
+							guns.superGun, 
+							rnd.nextInt(1000)+1000, 
+							mainColor,
+							dopColor,
+					        false, 
+					        false, 
+					        false);			    		
+		    	}   		
+				add(_transport);	
+	         }	    	
+	    	return -1;	    		 
+		}	    	    
+	    public  void   del (int num) 
+	    { 	
+	    	 int score=0;	    	    
+	    	for (int i = 0; i < this._places.length; i++)
+	        {
+	            if (!this.CheckFreePlace(i))
+	            {
+	            	score++;
+	            }
+	        }
+	    		num = (score/num)-1;
+	    	
+	    	for (int i = _places.length-1; i > num; i--) 
+	    	{
+	    		 if(_places[i]!=null)
+		          _places[i] = null;		            
+		     }  					
+	     }
+	    
+	    private boolean CheckFreePlace(int index)
+	    {
+	        return _places[index] == null;
+	    }
+	    
+	    public void Draw(Graphics g)
+	    {
+	        DrawMarking(g); 
+	        for (int i = 0; i < _places.length; i++)
+	        {
+	            if (!CheckFreePlace(i))
+	            {  
+	                _places[i].DrawTransport(g);     
+	            }
+	        }
+	     }
+	      
+	    private void DrawMarking(Graphics g)
+	    {
+	        g.setColor(Color.BLACK);
+	       
+	        g.drawRect(0, 0, (_places.length / 5) * _placeSizeWidth, 700);
+	        for (int i = 0; i < _places.length / 5; i++)
+	        {
+	            for (int j = 0; j < 6; ++j)
+	            {            
+	                g.drawLine(i * _placeSizeWidth, j * _placeSizeHeight-7,
+	                i * _placeSizeWidth + 110, j * _placeSizeHeight-7);
+	            }
+	            g.drawLine(i * _placeSizeWidth, 0, i * _placeSizeWidth, 700);
+	        }
+	    }	    		
+}
